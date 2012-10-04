b80b0b0ad41494e543f2a7e96434b68eb86609ed
Areski Belaid <areski@gmail.com>
2012-10-04 13:12:51 +0200

    update version and setup.py fix attempt for pypi

--- audiofield/__init__.py
+++ audiofield/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (0, 4, 8, "final", 0)
+VERSION = (0, 4, 9, "final", 0)
 
 
 def get_version():
--- setup.py
+++ setup.py
@@ -1,196 +1,26 @@
-#
-# django-audiofield License
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this file,
-# You can obtain one at http://mozilla.org/MPL/2.0/.
-#
-# Copyright (C) 2011-2012 Star2Billing S.L.
-#
-# The Initial Developer of the Original Code is
-# Arezqui Belaid <info@star2billing.com>
-#
-from setuptools import setup
-from setuptools import find_packages
-import os
-import codecs
-from fnmatch import fnmatchcase
-from distutils.util import convert_path
-import sys
-import re
+from setuptools import setup, find_packages
 import audiofield
+import os
 
 
-version = audiofield.__version__
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-
-def read(*parts):
-    return codecs.open(os.path.join(os.path.dirname(__file__), *parts)).read()
-
-
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk('audiofield'):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'):
-            del dirnames[i]
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-    elif filenames:
-        prefix = dirpath[12:]
-        for f in filenames:
-            data_files.append(os.path.join(prefix, f))
-
-
-def parse_requirements(file_name):
-    requirements = []
-    for line in open(file_name, 'r').read().split('\n'):
-        if re.match(r'(\s*#)|(\s*$)', line):
-            continue
-        if re.match(r'\s*-e\s+', line):
-            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
-        elif re.match(r'(\s*git)|(\s*hg)', line):
-            pass
-        else:
-            requirements.append(line)
-    return requirements
-
-
-def parse_dependency_links(file_name, install_flag=False):
-    dependency_links = []
-    for line in open(file_name, 'r').read().split('\n'):
-        if re.match(r'\s*-e\s+', line):
-            dependency_links.append(re.sub(r'\s*-e\s+', '', line))
-        if re.match(r'(\s*git)|(\s*hg)', line):
-            if install_flag == True:
-                line_arr = line.split('/')
-                line_arr_length = len(line.split('/'))
-                pck_name = line_arr[line_arr_length - 1].split('.git')
-                if len(pck_name) == 2:
-                    os.system('pip install -f %s %s' % (pck_name[0], line))
-                if len(pck_name) == 1:
-                    os.system('pip install -f %s %s' % (pck_name, line))
-    return dependency_links
-
-
-# Provided as an attribute, so you can append to these instead
-# of replicating them:
-standard_exclude = ('*.py', '*.pyc', '*$py.class', '*~', '.*', '*.bak')
-standard_exclude_directories = ('.*', 'CVS', '_darcs', './build',
-                                './dist', 'EGG-INFO', '*.egg-info')
-
-
-# (c) 2005 Ian Bicking and contributors; written for Paste (http://pythonpaste.org)
-# Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
-# Note: you may want to copy this into your setup.py file verbatim, as
-# you can't import this from another package, when you don't know if
-# that package is installed yet.
-def find_package_data(where='.', package='',
-                      exclude=standard_exclude,
-                      exclude_directories=standard_exclude_directories,
-                      only_in_packages=True,
-                      show_ignored=False):
-    """
-    Return a dictionary suitable for use in ``package_data``
-    in a distutils ``setup.py`` file.
-
-    The dictionary looks like::
-
-        {'package': [files]}
-
-    Where ``files`` is a list of all the files in that package that
-    don't match anything in ``exclude``.
-
-    If ``only_in_packages`` is true, then top-level directories that
-    are not packages won't be included (but directories under packages
-    will).
-
-    Directories matching any pattern in ``exclude_directories`` will
-    be ignored; by default directories with leading ``.``, ``CVS``,
-    and ``_darcs`` will be ignored.
-
-    If ``show_ignored`` is true, then all the files that aren't
-    included in package data are shown on stderr (for debugging
-    purposes).
-
-    Note patterns use wildcards, or can be exact paths (including
-    leading ``./``), and all searching is case-insensitive.
-    """
-
-    out = {}
-    stack = [(convert_path(where), '', package, only_in_packages)]
-    while stack:
-        where, prefix, package, only_in_packages = stack.pop(0)
-        for name in os.listdir(where):
-            fn = os.path.join(where, name)
-            if os.path.isdir(fn):
-                bad_name = False
-                for pattern in exclude_directories:
-                    if (fnmatchcase(name, pattern) or fn.lower() == pattern.lower()):
-                        bad_name = True
-                        if show_ignored:
-                            print >> sys.stderr, (
-                                "Directory %s ignored by pattern %s"
-                                % (fn, pattern))
-                        break
-                if bad_name:
-                    continue
-                if (os.path.isfile(os.path.join(fn, '__init__.py')) and not prefix):
-                    if not package:
-                        new_package = name
-                    else:
-                        new_package = package + '.' + name
-                    stack.append((fn, '', new_package, False))
-                else:
-                    stack.append((fn, prefix + name + '/', package, only_in_packages))
-            elif package or not only_in_packages:
-                # is a file
-                bad_name = False
-                for pattern in exclude:
-                    if (fnmatchcase(name, pattern) or fn.lower() == pattern.lower()):
-                        bad_name = True
-                        if show_ignored:
-                            print >> sys.stderr, (
-                                "File %s ignored by pattern %s"
-                                % (fn, pattern))
-                        break
-                if bad_name:
-                    continue
-                out.setdefault(package, []).append(prefix + name)
-    return out
-
-
-install_flag = False
-if sys.argv[1] == "install":
-    install_flag = True
+README = read('README.rst')
 
 setup(
     name='django-audiofield',
-    version=version,
+    version=audiofield.__version__,
     description='Django application which allows audio file upload and conversion to mp3, wav and ogg format',
-    long_description=read('README.rst'),
+    long_description=README,
+    url='http://github.com/Star2Billing/django-audiofield',
     author='Belaid Arezqui',
     author_email='areski@gmail.com',
-    url='http://github.com/Star2Billing/django-audiofield',
-    download_url='https://github.com/Star2Billing/django-audiofield/tarball/master',
-    zip_safe=False,
-    #package_dir={'audiofield': 'audiofield'},
-    #packages=find_packages(),
-    packages=['audiofield'],
-    include_package_data = True,
-    package_data=find_package_data(),
-    #package_data={'audiofield': data_files},
-    install_requires=parse_requirements('audiofield/requirements.txt'),
-    dependency_links=parse_dependency_links('audiofield/requirements.txt',
-                                            install_flag),
     license='MIT License',
+    zip_safe=False,
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={},
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
