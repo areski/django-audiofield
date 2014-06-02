eb47a3dbe1cad1fd4657ed901094d548b5db016f
areski <areski@gmail.com>
2014-06-02 13:44:39 +0200

    fix update_version script

--- update_version.sh
+++ update_version.sh
@@ -4,11 +4,11 @@
 #
 
 git flow release start v$1
-sed -i -e "s/__version__ = '.*'/__version__ = '$1'/g" django_audiofield/__init__.py
+sed -i -e "s/__version__ = '.*'/__version__ = '$1'/g" audiofield/__init__.py
 #rm -rf docs/html
 #python setup.py develop
 #make docs
-#git commit docs django_audiofield/__init__.py -m "Update to version v$1"
+#git commit docs audiofield/__init__.py -m "Update to version v$1"
 git commit -a -m "Update to version v$1"
 git flow release finish v$1
 python setup.py sdist upload -r pypi
