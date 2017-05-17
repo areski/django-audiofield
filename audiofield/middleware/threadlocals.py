6e7d607f33cd6de60bafbe04eb252c693e24b150
Evan Whitbeck <evan@ube-kosan.com>
2017-05-17 18:12:49 -0400

    Now that the server can be run, using the previous MiddlewareMixin module from django.utils.deprecation allows for forwards and backwards compatibility but it is not a longterm solution - is good as a temporary fix until 2.0 when the fix is slated to be removed

--- audiofield/middleware/threadlocals.py
+++ audiofield/middleware/threadlocals.py
@@ -11,38 +11,20 @@
 # Arezqui Belaid <info@star2billing.com>
 #
 
+from django.utils.deprecation import MiddlewareMixin
 import threading
 
 _thread_locals = threading.local()
 
-# middleware updated to be compatible with Django 1.10+ 5/16/2017 per - https://docs.djangoproject.com/en/1.11/topics/http/middleware/#upgrading-middleware
-# updates on lines 22 - 23 and 35 - 41 on 5/16/2017 are to attempt to add back compatibility with Django 1.9 and prev. - testing needed
-# Issue submitted to audiofield project on github at: https://github.com/areski/django-audiofield/issues/27
 
 def get_current_request():
     return getattr(_thread_locals, 'request', None)
 
-class ThreadLocals(object): 
-	"""
-	Middleware that gets various objects from the
-	request object and saves them in thread local storage.
-	"""
-	def process_request(self, request):
-		_thread_locals.request = request	
 
-	def __init__(self, get_response):
-		self.get_response = get_response
-
-	def __call__(self, request):
-		response = None
-
-		_thread_locals.request = request
-		
-		if hasattr(self, 'process_request'):
-			response = self.process_request(request)
-		if not response:
-			response = self.process_request(request)
-
-		self.get_response(request)
-
-		return response
+class ThreadLocals(MiddlewareMixin):
+    """
+    Middleware that gets various objects from the
+    request object and saves them in thread local storage.
+    """
+    def process_request(self, request):
+        _thread_locals.request = request
