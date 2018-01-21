9e743174571bc4ba67e9e960ff879ec5289040d9
Bulat Khasanov <afti@yandex.ru>
2018-01-21 11:57:56 +0300

    Update ThreadLocals middleware for <1.10 compatibility.

--- audiofield/middleware/threadlocals.py
+++ audiofield/middleware/threadlocals.py
@@ -11,7 +11,6 @@
 # Arezqui Belaid <info@star2billing.com>
 #
 
-from django.utils.deprecation import MiddlewareMixin
 import threading
 
 _thread_locals = threading.local()
@@ -21,10 +20,24 @@ def get_current_request():
     return getattr(_thread_locals, 'request', None)
 
 
-class ThreadLocals(MiddlewareMixin):
+class ThreadLocals(object):
     """
     Middleware that gets various objects from the
     request object and saves them in thread local storage.
     """
+
+    def __init__(self, get_response=None):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = None
+        if hasattr(self, 'process_request'):
+            response = self.process_request(request)
+        if not response:
+            response = self.get_response(request)
+        if hasattr(self, 'process_response'):
+            response = self.process_response(request, response)
+        return response
+
     def process_request(self, request):
         _thread_locals.request = request
